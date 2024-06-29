9ab5239a962e56b217ae95234aac14e1efd29c76
JA8866 <109983530+JA8866@users.noreply.github.com>
2024-06-29 18:39:30 -0500

    initialized part3 project for full stack open

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # fullStackOpen-2024-part3
+This repository contains all coursework for part3 of Full Stack Open, started in 2024. 
---
+++ index.js
@@ -0,0 +1,97 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+let persons = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+];
+
+// app.get('/', (request, response) => {
+//     response.send('<h1>Hello World!</h1>');
+// });
+  
+app.get('/api/persons', (request, response) => {
+    response.json(persons);
+});
+
+// app.get('/api/notes/:id', (request, response) => {
+//     const id = Number(request.params.id);
+//     const note = notes.find(note => note.id === id);
+
+//     if (note) {
+//       response.json(note);
+//     } else {
+//       response.status(404).end();
+//     };
+// });
+
+// app.delete('/api/notes/:id', (request, response) => {
+//     const id = Number(request.params.id);
+//     notes = notes.filter(note => note.id !== id);
+//     response.status(204).end();
+// });
+
+// const generateId = () => {
+//     const maxId = notes.length > 0
+//       ? Math.max(...notes.map(n => Number(n.id)))
+//       : 0;
+//     return String(maxId + 1);
+// };
+
+// app.post('/api/notes', (request, response) => {
+//     const body = request.body;
+
+//     if (!body.content) {
+//       return response.status(400).json({ 
+//         error: 'content missing'
+//       });
+//     };
+
+//     const note = {
+//       content: body.content,
+//       important: Boolean(body.important) || false,
+//       id: generateId(),
+//     };
+
+//     notes = notes.concat(note);
+//     response.json(note);
+// });
+
+// const requestLogger = (request, response, next) => {
+//     console.log('Method:', request.method);
+//     console.log('Path:  ', request.path);
+//     console.log('Body:  ', request.body);
+//     console.log('---');
+//     next();
+// };
+// app.use(requestLogger);
+
+// const unknownEndpoint = (request, response) => {
+//     response.status(404).send({ error: 'unknown endpoint' });
+// };
+// app.use(unknownEndpoint);
+
+const PORT = 3001;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
