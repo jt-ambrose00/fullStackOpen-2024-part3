64014383d42d0986878897d623a2ae4daea5ebb8
JA8866 <109983530+JA8866@users.noreply.github.com>
2024-06-29 19:09:23 -0500

    finished 3.2

--- index.js
+++ index.js
@@ -25,13 +25,18 @@ let persons = [
       "number": "39-23-6423122"
     }
 ];
-
-// app.get('/', (request, response) => {
-//     response.send('<h1>Hello World!</h1>');
-// });
   
 app.get('/api/persons', (request, response) => {
     response.json(persons);
+});
+
+app.get('/info', (request, response) => {
+    const dateTime = new Date();
+
+    response.send(
+        `<p>phonebook has info for ${persons.length} people</p>` +
+        `<p>${dateTime}</p>`
+    );
 });
 
 // app.get('/api/notes/:id', (request, response) => {
