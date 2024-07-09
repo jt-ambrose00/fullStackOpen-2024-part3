7cc67326d8bf10fa2fc7319d46dc42b450de2cd4
JA8866 <109983530+JA8866@users.noreply.github.com>
2024-07-09 15:08:31 -0500

    finished 3.14

--- index.js
+++ index.js
@@ -73,34 +73,24 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end();
 });
 
-const generateId = () => {
-    const randomId = Math.floor(Math.random() * 99999);
-    return String(randomId);
-};
-
 app.post('/api/persons', (request, response) => {
     const body = request.body;
 
-    if (!body.name || !body.number) {
+    if (body.name === undefined || body.number === undefined) {
       return response.status(400).json({ 
         error: 'name or number missing'
       });
     };
 
-    if (persons.find(person => person.name === body.name)) {
-        return response.status(400).json({ 
-            error: 'name must be unique'
-        });
-    };
-
-    const person = {
-        id: generateId(),
+    const person = new Person({
         name: body.name,
-        number: body.number
-    };
+        number: body.number,
+    });
 
-    persons = persons.concat(person);
-    response.json(person);
+    person.save()
+      .then(savedPerson => {
+        response.json(savedPerson);
+      });
 });
 
 // const unknownEndpoint = (request, response) => {
