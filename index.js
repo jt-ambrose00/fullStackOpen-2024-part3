00f62a412442d484ae1048a3c72344ae7610bc2c
JA8866 <109983530+JA8866@users.noreply.github.com>
2024-07-10 12:23:45 -0500

    finished 3.15

--- index.js
+++ index.js
@@ -94,11 +94,6 @@ app.post('/api/persons', (request, response) => {
       });
 });
 
-// const unknownEndpoint = (request, response) => {
-//     response.status(404).send({ error: 'unknown endpoint' });
-// };
-// app.use(unknownEndpoint);
-
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
