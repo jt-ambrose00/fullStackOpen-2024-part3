874df66827a57ee5ffd686a0f47d583dcda1214d
JA8866 <109983530+JA8866@users.noreply.github.com>
2024-07-10 15:25:44 -0500

    finished 3.17

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ app.post('*', morgan(
 ));
 app.get('*', morgan('tiny'));
 app.delete('*', morgan('tiny'));
+app.put('*', morgan('tiny'));
 
 let persons = [
     {
@@ -77,32 +78,46 @@ app.get('/api/persons/:id', (request, response) => {
 });
 
 app.delete('/api/persons/:id', (request, response, next) => {
-    Person.findByIdAndDelete(request.params.id)
-      .then(result => {
-        response.status(204).end();
-      })
-      .catch(error => next(error));
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end();
+    })
+    .catch(error => next(error));
 });
 
 app.post('/api/persons', (request, response) => {
-    const body = request.body;
+  const body = request.body;
 
-    if (body.name === undefined || body.number === undefined) {
-      return response.status(400).json({ 
-        error: 'name or number missing'
-      });
-    };
-
-    const person = new Person({
-        name: body.name,
-        number: body.number,
+  if (body.name === undefined || body.number === undefined) {
+    return response.status(400).json({ 
+      error: 'name or number missing'
     });
+  };
 
-    person.save()
-      .then(savedPerson => {
-        response.json(savedPerson);
-      })
-      .catch(error => next(error));
+  const person = new Person({
+      name: body.name,
+      number: body.number,
+  });
+
+  person.save()
+    .then(savedPerson => {
+      response.json(savedPerson);
+    })
+    .catch(error => next(error));
+});
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body;
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson);
+    })
+    .catch(error => next(error));
 });
 
 const errorHandler = (error, request, response, next) => {
